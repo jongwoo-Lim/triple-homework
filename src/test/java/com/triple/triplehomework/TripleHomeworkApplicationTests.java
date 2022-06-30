2e01c4dd2cdaf5449f26efe0c4f4cf553caab425
jongwoolim <whddn528@naver.com>
2022-06-30 09:24:35 +0900

    init commit

---
+++ src/test/java/com/triple/triplehomework/TripleHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.triplehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
