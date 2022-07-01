746749df8c04b58d1e15441c02640fed463316e5
jongwoolim <whddn528@naver.com>
2022-07-01 11:32:00 +0900

    test: 리뷰 작성 존재 체크 테스트 추가

--- src/test/java/com/triple/triplehomework/repository/ReviewRepositoryTest.java
+++ src/test/java/com/triple/triplehomework/repository/ReviewRepositoryTest.java
@@ -12,6 +12,39 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 class ReviewRepositoryTest extends BaseRepositoryTest {
 
     @Test
+    @DisplayName("유저가 한 장소의 리뷰 0개 체크 테스트")
+    public void countReview_zero_test() throws Exception{
+        // Given
+        Member admin = createMember();
+        Place place = createPlace(admin);
+
+        Member member = createMember();
+        // When
+        boolean result = reviewRepository.existsReviewByUserIdAndPlace(member.getUserId(), place);
+
+        // Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("유저가 한 장소의 리뷰 1개 체크 테스트")
+    public void countReview_one_test() throws Exception{
+        // Given
+        Member admin = createMember();
+        Place place = createPlace(admin);
+
+        Member member = createMember();
+        Review review = createReview(member, place);
+        Review savedReview = reviewRepository.save(review);
+
+        // When
+        boolean result = reviewRepository.existsReviewByUserIdAndPlace(member.getUserId(), place);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
     @DisplayName("리뷰 등록 테스트")
     public void createReview_test() throws Exception{
         // Given
@@ -30,5 +63,4 @@ class ReviewRepositoryTest extends BaseRepositoryTest {
         assertThat(savedReview.getReviewId()).isNotNull();
         assertThat(savedReview.getAction()).isEqualTo(ReviewActionCode.ADD);
     }
-
 }
