0e5d9302b174d058bdc9195a93609e99d7912c7b
jongwoolim <whddn528@naver.com>
2022-07-01 15:06:41 +0900

    feat: Review, Member 리포지토리 쿼리 메서드 추가

--- src/main/java/com/triple/triplehomework/repository/MemberRepository.java
+++ src/main/java/com/triple/triplehomework/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.triple.triplehomework.repository;
 import com.triple.triplehomework.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(UUID userId);
 }
--- src/main/java/com/triple/triplehomework/repository/ReviewRepository.java
+++ src/main/java/com/triple/triplehomework/repository/ReviewRepository.java
@@ -2,11 +2,21 @@ package com.triple.triplehomework.repository;
 
 import com.triple.triplehomework.entity.place.Place;
 import com.triple.triplehomework.entity.review.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
     boolean existsReviewByUserIdAndPlace(UUID userId, Place place);
+
+    @Query("select p " +
+            "from Place p " +
+            "inner join Review r on p.placeId = r.place.placeId " +
+            "where p.placeId = :placeId ")
+    List<Place> existsReviewByPlace(@Param("placeId") UUID placeId, Pageable pageable);
 }
