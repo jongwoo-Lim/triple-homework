aa5a6e687ad4ed8403cf096d57fe15317f0d37d2
limjongwoo <whddn528@naver.com>
2022-07-02 17:36:33 +0900

    feat: 포인트 내역 발생 근거 추가

--- src/main/java/com/triple/triplehomework/common/code/PointOccurCode.java
+++ src/main/java/com/triple/triplehomework/common/code/PointOccurCode.java
@@ -3,7 +3,9 @@ package com.triple.triplehomework.common.code;
 public enum PointOccurCode {
     REVIEW("리뷰 작성"),
     PHOTO("사진 첨부"),
-    BONUS("첫 리뷰 작성");
+    BONUS("첫 리뷰 작성"),
+
+    PHOTO_ALL_REMOVED("해당 리뷰 첨부 파일 모두 삭제");
 
     private final String occurCause;
 
